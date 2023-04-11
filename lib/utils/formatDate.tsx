1e64139d86819e96255638abda50275daae63213
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2023-04-11 16:00:52 +0530

    added type any to formatDate

--- lib/utils/formatDate.tsx
+++ lib/utils/formatDate.tsx
@@ -1,11 +1,11 @@
 import siteMetadata from "@/data/metadata";
 
-const formatDate = (date) => {
+const formatDate = (date: any) => {
   const options = {
     year: "numeric",
     month: "long",
     day: "numeric",
-  };
+  } as any;
   const now = new Date(date).toLocaleDateString(siteMetadata.locale, options);
 
   return now;
